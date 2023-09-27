f7d300ff0445da78ef784b5b964038d2b1d29052
Esteban <csarlezcano@gmail.com>
2023-09-26 22:22:24 -0400

    Primer commit

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# ChatGPT + Whatsapp Bot + Python
+
+Impulsa tu negocio con un bot usando las apis oficiales de whatsapp.
+pueden ver el video paso a paso en el siguiente enlace: [https://youtu.be/puYWiZDJnL0](https://youtu.be/S9Ev3DFXBEk)
+
+## Descarga el proyecto
+
+```bash
+git clone https://github.com/JPierr3/bigdateros-chagpt-whatsappbot.git
+```
+    
+Modifica el archivo sett.py con los token de meta y openAI
---
+++ sett.py
@@ -0,0 +1,25 @@
+token = 'EAALQZAwhK5UsBO2YOX3IMLwLPDBYmS2rZBAHnG8ymCMJATuqY9yaUNZAGJ7jh4dXzGitlpTNKAAIyvxilvpZBBmoEViFcyNeeBZBF2OPVlZBdbBO7ZB6XP7OWBbSEqFGGaEKa7PzoDtD2dbwtoZCYZCsmKxfTuNumAYAaSnVu0CdwyijUIu9ZBmPFqD5IB0mcZCjSaX'
+
+whatsapp_token = 'EAALQZAwhK5UsBO2YOX3IMLwLPDBYmS2rZBAHnG8ymCMJATuqY9yaUNZAGJ7jh4dXzGitlpTNKAAIyvxilvpZBBmoEViFcyNeeBZBF2OPVlZBdbBO7ZB6XP7OWBbSEqFGGaEKa7PzoDtD2dbwtoZCYZCsmKxfTuNumAYAaSnVu0CdwyijUIu9ZBmPFqD5IB0mcZCjSaX'
+
+whatsapp_url = 'https://graph.facebook.com/v17.0/150948954757622/messages'
+
+stickers = {
+    "poyo_feliz": 984778742532668,
+    "perro_traje": 1009219236749949,
+    "perro_triste": 982264672785815,
+    "pedro_pascal_love": 801721017874258,
+    "pelfet": 3127736384038169,
+    "anotado": 24039533498978939,
+    "gato_festejando": 1736736493414401,
+    "okis": 268811655677102,
+    "cachetada": 275511571531644,
+    "gato_juzgando": 107235069063072,
+    "chicorita": 3431648470417135,
+    "gato_triste": 210492141865964,
+    "gato_cansado": 1021308728970759
+}
+
+document_url = "https://www.africau.edu/images/default/sample.pdf"
+
+openai_key = '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'
